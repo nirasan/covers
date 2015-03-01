632922339a98a1ac73406cb89e13a0268765d2d0
nirasan <niranirao@gmail.com>
2015-03-01 13:01:57 +0900

    add browse_node_id test

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Book, type: :model do
         Book.request_amazon(keyword: "test")
         expect(Book.count).to eq 10
       end
+      it "use browse_node_id" do
+        expect(Book.count).to eq 0
+        Book.request_amazon(browse_node_id: 2250738051)
+        expect(Book.count).to eq 10
+      end
     end
   end
 end
